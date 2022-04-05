436da5bd41b4bf580fa49e7d86fda053ffd80553
rejaul-islam1 <rejaul.hero@gmail.com>
2022-04-05 13:56:56 +0600

    page not found added

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PageNotFound.css';
+
+const PageNotFound = () => {
+    return (
+        <div className='not-found'>
+            <div>
+                <h2>Page Not Found</h2>
+                <h1>404</h1>
+            </div>
+        </div>
+    );
+};
+
+export default PageNotFound;
